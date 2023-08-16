2ad476c96c195899cf7cd5ba5dc70ee22541748b
Roman <71276897+romangoempire@users.noreply.github.com>
2023-08-16 16:26:42 +0200

    dealer behavior as player implemented

--- perfect_strategie.py
+++ perfect_strategie.py
@@ -84,7 +84,7 @@ def print_hand(hand, is_dealer=False, is_hidden=False):
                 string += f" -> {sum(hands)}\n"
         else:
             string += "Player: " + ", ".join(str(card) for card in hand) + f" -> {sum(hand)}"
-    print(string)
+    # print(string)
 
 
 def get_value_of_hand(hand, is_dealer=False):
@@ -103,7 +103,7 @@ def get_correct_decision(player_hand, dealer_value):
             player_value == 16 and dealer_value == (9 or 10 or 11) or player_value == 15 and dealer_value == 10):
         return "Surrender"
     decision = ""
-    if player_hand[0] == player_hand[1] and has_two_cards(player_hand) and hands < 4:
+    if player_hand[0] == player_hand[1] and has_two_cards(player_hand) and HANDS < 4:
         decision = get_split_decision(player_hand, dealer_value)
     if decision == "Yes" or decision == "Yes/No":
         return "Yes"
@@ -113,6 +113,8 @@ def get_correct_decision(player_hand, dealer_value):
         decision = get_hard_decision(player_hand, dealer_value)
         if decision == "Double":
             if has_two_cards(player_hand):
+                global BET
+                BET *= 2
                 return "Double"
             else:
                 return "Hit"
@@ -122,7 +124,8 @@ def get_correct_decision(player_hand, dealer_value):
 
 def get_hard_decision(hand, dealer_value):
     your_value = get_value_of_hand(hand)
-
+    if your_value == 16 and TRUE_COUNT > 0:
+        return "Stand"
     if your_value > 17:
         return "Stand"
     if your_value < 8:
@@ -166,25 +169,25 @@ def player_has_blackjack(hand):
 
 
 def result_of_split(player_hand, dealer_hand):
-    global hands
-    print("Split")
+    global HANDS
+    # print("Split")
     hand1 = [player_hand[0], deck.pop()]
     hand2 = [player_hand[1], deck.pop()]
-    hands += 1
-    print_hand(hand1)
-    print_hand(hand2)
-    print()
+    HANDS += 1
+    # print_hand(hand1)
+    # print_hand(hand2)
+    # print()
     hand1 = play_hand_player(hand1, dealer_hand)
     hand2 = play_hand_player(hand2, dealer_hand)
     return [hand1, hand2]
 
 
 def update_count(card):
-    global count
+    global COUNT
     if card < 7:
-        count += 1
+        COUNT += 1
     elif card > 9:
-        count -= 1
+        COUNT -= 1
 
 
 def result_of_hit(player_hand, dealer_hand):
@@ -194,9 +197,9 @@ def result_of_hit(player_hand, dealer_hand):
         player_hand = change_aceto1(player_hand)
     elif get_value_of_hand(player_hand) > 21:
         return player_hand
-    print_hand(player_hand)
+    # print_hand(player_hand)
     decision = get_correct_decision(player_hand, dealer_value)
-    print(decision)
+    # print(decision)
     if decision == "Stand":
         return player_hand
     elif decision == "Hit":
@@ -213,12 +216,12 @@ def result_of_double(player_hand):
 
 
 def play_hand_player(player_hand, dealer_hand):
-    print("Player turn")
-    print_hand(dealer_hand, True)
-    print_hand(player_hand)
+    # print("Player turn")
+    # print_hand(dealer_hand, True)
+    # print_hand(player_hand)
     dealer_value = get_value_of_hand(dealer_hand, True)
     decision = get_correct_decision(player_hand, dealer_value)
-    print(decision)
+    # print(decision)
     match decision:
         case "Surrender":
             return None
@@ -236,6 +239,27 @@ def play_hand_player(player_hand, dealer_hand):
             return result_of_split(player_hand, dealer_hand)
 
 
+def play_hand_player_2(player_hand):
+    if get_value_of_hand(player_hand) == 21:
+        return player_hand
+    if get_value_of_hand(player_hand) > 21 and has_ace(player_hand):
+        player_hand = change_aceto1(player_hand)
+    elif get_value_of_hand(player_hand) > 21:
+        return player_hand
+    if get_value_of_hand(player_hand) == 16 and TRUE_COUNT > 0:
+        return player_hand
+    if get_value_of_hand(player_hand) == 11 and has_two_cards(player_hand):
+        global BET
+        BET *= 2
+        add_card(player_hand)
+        return player_hand
+    while get_value_of_hand(player_hand) < 17:
+        add_card(player_hand)
+        # print_hand(dealer_hand, True, True)
+    # print()
+    return player_hand
+
+
 def change_aceto1(hand):
     index = hand.index(11)
     hand[index] = 1
@@ -243,7 +267,7 @@ def change_aceto1(hand):
 
 
 def play_dealer_hand(dealer_hand):
-    print("Dealer's turn")
+    # print("Dealer's turn")
     if get_value_of_hand(dealer_hand) == 21:
         return dealer_hand
     if get_value_of_hand(dealer_hand) > 21 and has_ace(dealer_hand):
@@ -252,63 +276,64 @@ def play_dealer_hand(dealer_hand):
         return dealer_hand
     while get_value_of_hand(dealer_hand) < 17:
         add_card(dealer_hand)
-        print_hand(dealer_hand, True, True)
-    print()
+        # print_hand(dealer_hand, True, True)
+    # print()
     return dealer_hand
 
 
 def decide_winner(hand, final_hand_dealer):
-    global bankroll
+    global BANKROLL
     # player busts
     if get_value_of_hand(hand) > 21:
-        print("Player busts")
-        bankroll -= bet
-        bankroll_history.append(bankroll)
+        # print("Player busts")
+        BANKROLL -= BET
+        BANKROLL_HISTORY.append(BANKROLL)
         return "Dealer"
     # if both have the same value then no one wins
     if get_value_of_hand(hand) == get_value_of_hand(final_hand_dealer):
-        print("Push")
+        # print("Push")
         return "Push"
     # player has higher value than dealer or dealer busts
     if get_value_of_hand(hand) > get_value_of_hand(final_hand_dealer) or get_value_of_hand(final_hand_dealer) > 21:
         # Larger payout for blackjack
         if player_has_blackjack(hand) and has_two_cards(hand):
-            print("Player has blackjack")
-            bankroll += bet * 1.5
-            bankroll_history.append(bankroll)
+            # print("Player has blackjack")
+            BANKROLL += BET * 1.5
+            BANKROLL_HISTORY.append(BANKROLL)
         # Normal payout
         else:
-            print("Player wins")
-            bankroll += bet
-            bankroll_history.append(bankroll)
+            # print("Player wins")
+            BANKROLL += BET
+            BANKROLL_HISTORY.append(BANKROLL)
         return "Player"
     # dealer has higher value than player
     else:
-        print("Dealer wins")
-        bankroll -= bet
-        bankroll_history.append(bankroll)
+        # print("Dealer wins")
+        BANKROLL -= BET
+        BANKROLL_HISTORY.append(BANKROLL)
         return "Dealer"
 
 
-initial_bankroll = 10000
-bankroll = 10000
-bankroll_history = []
-rounds = 0
-bet = 1
-hands = 1
-count = 0
+INITIAL_BANKROLL = 100000
+BANKROLL = 100000
+BANKROLL_HISTORY = []
+ROUNDS = 0
+BET = 1
+HANDS = 1
+COUNT = 0
+TRUE_COUNT = 0
 
 
 def get_splitted_winner(hand, final_hand_dealer):
-    global bankroll
+    global BANKROLL
     if hand is None:
-        bankroll -= bet / 2
-        bankroll_history.append(bankroll)
-        print("Surrender")
+        BANKROLL -= BET / 2
+        BANKROLL_HISTORY.append(BANKROLL)
+        # print("Surrender")
         return
     if type(hand[0]) is int and type(hand[1]) is int:
-        print_hand(hand)
-        print_hand(final_hand_dealer, True, True)
+        # print_hand(hand)
+        # print_hand(final_hand_dealer, True, True)
         decide_winner(hand, final_hand_dealer)
     else:
         for h in hand:
@@ -316,50 +341,66 @@ def get_splitted_winner(hand, final_hand_dealer):
 
 
 def play_round(amount):
-    global bankroll
+    global BANKROLL
     global deck
-    global bet
-    global rounds
-    global hands
-    global count
+    global BET
+    global ROUNDS
+    global HANDS
+    global COUNT
+    global TRUE_COUNT
 
     for i in range(amount):
-        print(f'bet: {bet}')
-        if bankroll <= 0:
-            print("You lost all your money")
+        # print(f'bet: {bet}')
+        if BANKROLL <= 0:
+            # print("You lost all your money")
             break
         if len(deck) < 25:
             deck = create_six_deck()
-            count = 0
+            COUNT = 0
         player_hand = get_hand()
         dealer_hand = get_hand()
 
+        # if dealer_hand[0] == 11 and TRUE_COUNT >= 5:
+        #     # print("Insurance")
+        #     BANKROLL -= BET / 2
+        #     if dealer_hand[1] == 10:
+        #         BANKROLL += BET
+        #         # print("Insurance won")
+        #     else:
+        #         # print("Insurance lost")
+        #         pass
         final_hand_player = play_hand_player(player_hand, dealer_hand)
+        # final_hand_player = play_hand_player_2(player_hand)
 
         if final_hand_player is None:
-            bankroll -= bet / 2
-            print("Surrender")
+            BANKROLL -= BET / 2
+            # print("Surrender")
             continue
 
         final_hand_dealer = play_dealer_hand(dealer_hand)
+
         if has_hand_been_split(final_hand_player):
             get_splitted_winner(final_hand_player, final_hand_dealer)
         else:
-            print_hand(final_hand_player)
-            print_hand(final_hand_dealer, True, True)
+            # print_hand(final_hand_player)
+            # print_hand(final_hand_dealer, True, True)
             decide_winner(final_hand_player, final_hand_dealer)
 
-        print("Bankroll: " + str(bankroll))
+        # print("Bankroll: " + str(bankroll))
         print("--------------------")
         print('Statistics')
-        hands = 1
-        rounds += 1
+        HANDS = 1
+        ROUNDS += 1
         deck_left = (len(deck) / 52)
-        true_count = count / deck_left
-        print(f'count: {count}')
+        TRUE_COUNT = COUNT / deck_left
+        print(f'count: {COUNT}')
         print(f'deck_left: {deck_left}')
-        print(f'True count: {true_count}')
-        bet = 1
+        print(f'True count: {TRUE_COUNT}')
+        BET = 1
+        # if TRUE_COUNT > 1:
+        #     BET = int(TRUE_COUNT) * 10
+        #     if BET > 100:
+        #         BET = 100
         print("--------------------")
 
 
@@ -369,7 +410,7 @@ def plot_history(backroll_history):
 
 
 if __name__ == '__main__':
-    play_round(1000000)
-    plot_history(bankroll_history)
-    print(f"rounds: {rounds}")
-    print(f"average loss per round: {initial_bankroll / rounds}")
+    play_round(1000000000)
+    plot_history(BANKROLL_HISTORY)
+    print(f"rounds: {ROUNDS}")
+    print(f"average loss per round: {INITIAL_BANKROLL / ROUNDS}")
